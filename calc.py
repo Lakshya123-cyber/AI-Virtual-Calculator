11aa1af1021e361e3728efa701e8c8a4283a0c53
Lakshya Raikwal <lakshya.raikwal@giis.edu.sg>
2021-12-18 15:23:49 +0800

    Add files via upload

---
+++ calc.py
@@ -0,0 +1,171 @@
+import cv2
+from cvzone.HandTrackingModule import (
+    HandDetector,
+)  # cvzone == 1.5.3 ; mediapipe == 0.8.9
+import time
+
+# Button Class so that we can reuse it
+class Button:
+    def __init__(self, pos, width, height, value):
+
+        self.pos = pos
+        self.width = width
+        self.height = height
+        self.value = value
+
+    def draw(self, img):
+
+        cv2.rectangle(
+            img,
+            self.pos,
+            (self.pos[0] + self.width, self.pos[1] + self.height),
+            (200, 200, 200),
+            cv2.FILLED,
+        )
+        cv2.rectangle(
+            img,
+            self.pos,
+            (self.pos[0] + self.width, self.pos[1] + self.height),
+            (50, 50, 50),
+            3,
+        )
+        cv2.putText(
+            img,
+            self.value,
+            (self.pos[0] + 40, self.pos[1] + 60),
+            cv2.FONT_HERSHEY_PLAIN,
+            2,
+            (50, 50, 50),
+            2,
+        )
+
+    def checkClick(self, x, y):
+        # x1 < x < x1 + width
+
+        if (
+            self.pos[0] < x < self.pos[0] + self.width
+            and self.pos[1] < y < self.pos[1] + self.height
+        ):
+            cv2.rectangle(
+                img,
+                self.pos,
+                (self.pos[0] + self.width, self.pos[1] + self.height),
+                (0, 0, 0),
+                cv2.FILLED,
+            )
+            cv2.putText(
+                img,
+                self.value,
+                (self.pos[0] + 25, self.pos[1] + 80),
+                cv2.FONT_HERSHEY_PLAIN,
+                5,
+                (255, 255, 255),
+                5,
+            )
+
+            return True
+
+        else:
+            return False
+
+
+# WebCam
+cap = cv2.VideoCapture(0)
+cap.set(3, 1280)  # width
+cap.set(4, 720)  # height
+detector = HandDetector(detectionCon=0.8, maxHands=1)
+
+# Creating Buttons
+buttonListValues = [
+    ["//", "%", "C", "/"],
+    ["7", "8", "9", "*"],
+    ["4", "5", "6", "-"],
+    ["1", "2", "3", "+"],
+    ["00", "0", ".", "="],
+]
+
+buttonList = []
+
+for x in range(4):
+    for y in range(5):
+        xpos = x * 100 + 800
+        ypos = y * 100 + 150
+        buttonList.append(Button((xpos, ypos), 100, 100, buttonListValues[y][x]))
+
+# Variables
+myEquation = ""
+delayCounter = 0
+
+# Loop
+while True:
+    # get image from webcam
+    success, img = cap.read()
+    img = cv2.flip(img, 1)  # 1 will flip it horizontally
+
+    # Detecting one hand
+    hands, img = detector.findHands(img, flipType=False)
+
+    # Draw all buttons
+    cv2.rectangle(
+        img,
+        (800, 50),
+        (800 + 400, 70 + 100),
+        (200, 200, 200),
+        cv2.FILLED,
+    )
+    cv2.rectangle(
+        img,
+        (800, 50),
+        (800 + 400, 70 + 100),
+        (50, 50, 50),
+        3,
+    )
+    for button in buttonList:
+        button.draw(img)
+
+    # Check for Hand
+    if hands:
+        lmList = hands[0]["lmList"]
+        length, _, _ = detector.findDistance(lmList[8], lmList[12], img)
+        # print(length)
+
+        x, y = lmList[8]
+
+        if length < 50:
+            for i, button in enumerate(buttonList):
+                if button.checkClick(x, y) and delayCounter == 0:
+                    myValue = buttonListValues[int(i % 5)][int(i / 5)]
+                    if myValue == "=":
+                        myEquation = str(eval(myEquation))
+
+                    # TODO: ADD FUNCTIONALITY FOR CLEARING THE VALUES IN THE RESULT BOX
+                    elif myValue == "C":
+                        myEquation = ""
+
+                    else:
+                        myEquation += myValue
+
+                    # time.sleep(0.2)
+                    delayCounter = 1
+
+    # Avoid duplicates
+    if delayCounter != 0:
+        delayCounter += 1
+
+        if delayCounter > 10:
+            delayCounter = 0
+
+    # Display the result
+    cv2.putText(
+        img,
+        myEquation,
+        (815, 120),
+        cv2.FONT_HERSHEY_PLAIN,
+        3,
+        (50, 50, 50),
+        3,
+    )
+
+    # Display image
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
